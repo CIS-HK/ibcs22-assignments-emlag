8552d90f99cb520f7c8983a7255228314613f7a6
emlag <edl926@g.harvard.edu>
2020-08-28 07:51:55 +0800

    add main

---
+++ Labs/app/src/main/java/edu/cis/labs_22/Lab1/MainClass.java
@@ -0,0 +1,8 @@
+package edu.cis.labs_22.Lab1;
+
+public class MainClass {
+    public static void main(String[] args) {
+        CISArrayList<String> m = new CISArrayList<>();
+        System.out.println("hello");
+    }
+}
---
+++ Labs/lib/src/main/java/edu/cis/lib/MyClass.kt
@@ -0,0 +1,4 @@
+package edu.cis.lib
+
+class MyClass {
+}
