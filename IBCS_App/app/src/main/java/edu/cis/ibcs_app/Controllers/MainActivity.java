6aabf65d3807313ec8009228e3453c311831807f
emlag <edl926@g.harvard.edu>
2020-08-28 06:53:54 +0800

    update todo 1

--- IBCS_App/app/src/main/java/edu/cis/ibcs_app/Controllers/MainActivity.java
+++ IBCS_App/app/src/main/java/edu/cis/ibcs_app/Controllers/MainActivity.java
@@ -24,28 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*
+         * Base code from android for this app
+         */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        /*
-         * Passing each menu ID as a set of Ids because each
-         * menu should be considered as top level destinations.
-         */
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        /*
+         * End of base code from android for this app
+         */
 
 
         //This is not great, for extra credit you can fix this so that network calls happen on a different thread
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+
+        //TODO 1: Tweet something!
         TwitterController myC = new TwitterController(this);
-//        myC.postTweet("hello world");
+        myC.postTweet("hello world"); //this will tweet to your account
 
     }
 
